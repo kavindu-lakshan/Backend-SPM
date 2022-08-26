325e95edb6bc6f2557893f3595426c30e5ea3f21
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-26 12:24:18 +0530

    created routes for authentication part

---
+++ Routes/auth_routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const authController = require('../Controllers/auth_controller');
+const router = express.Router();
+
+
+router.post('/signing', authController.login);
+router.patch('/update-account',authController.protect, authController.updateMyAccount);
+router.get('/current-user', authController.protect, authController.currentUser);
+router.get('/logout', authController.protect, authController.logout);
+
+module.exports = router;
