e978640ace89d83a4200687c30be02b5a9629ebd
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-28 15:11:53 +0530

    change route

--- Routes/AdminRoutes/staff_routes.js
+++ Routes/AdminRoutes/staff_routes.js
@@ -4,13 +4,13 @@ const authController = require("../../Controllers/auth_controller");
 const router = express.Router();
 
 //This api-resource route for update and delete specific student
-router.route("/")
+router.route("/staff")
     .get(staffController.allStaff)
     .post(staffController.createStaff)
 
 
-router.route("/:id")
-    .patch(authController.protect, authController.restrictTo('student'), staffController.updateStaff)
-    .delete(authController.protect, authController.restrictTo('student'), staffController.deleteStaff);
+router.route("/staff/:id")
+    .patch(authController.protect, authController.restrictTo('admin'), staffController.updateStaff)
+    .delete(authController.protect, authController.restrictTo('admin'), staffController.deleteStaff);
 
 module.exports = router;
