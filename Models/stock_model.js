2ec232ae3110e8752f16a49342c6df6d0937cddf
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-09-07 00:15:24 +0530

    make stock model

--- Models/stock_model.js
+++ Models/stock_model.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const stockSchema = new mongoose.Schema({
   item_code: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "",
+    ref: "Item",
+  },
+  available_stock: {
+    type: Number,
+    required: [true, "Please Enter Available Stocks"],
+  },
+  reservation: {
+    type: Number,
+    required: [true, "Please Enter Rservations"],
+  },
+  Availability: {
+    type: Boolean,
+    default: false,
+  },
+  last_update: {
+    type: Date,
+    default: Date.now,
   },
 });
+const Stock = mongoose.model("Stock", stockSchema);
+module.exports = Stock;
