772795d54c4e555a0432edbbb2c58cefbf4ea695
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-10-22 10:20:50 +0530

    changes

--- Routes/StockRoutes/stock_routes.js
+++ Routes/StockRoutes/stock_routes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const StockController = require("../../Controllers/StockController/stock_controller");
 const getStock = require("../../Controllers/StockController/getStock");
+const getRequest = require("../../Controllers/StockController/stock_controller");
 const AuthController = require("../../Controllers/auth_controller");
 
 router.post("/addstock", StockController.Stock);
 
 router.route("/getall").get(getStock);
+
+router.route("/getallRequest").get(getRequest.AllStockManagerRequests);
 
 router.route("/update/:id").put(StockController.UpdateStock);
 
